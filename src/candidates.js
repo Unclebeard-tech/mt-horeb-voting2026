078b03ca2c669fa94c16592f1235cd560dde84bf
Unclebeard-tech <rabbishharrison@gmail.com>
2026-07-16 17:16:51 +0300

    Update candidates.js

--- src/candidates.js
+++ src/candidates.js
@@ -1,60 +1,26 @@
-
 export const CANDIDATES = [
-  // PRESIDENT - 2
-  { id: "pres-1", name: "KAYLA ABEJA ONGOM", position: "PRESIDENT", photo: "/images/candidates/president-kayla-abeja-ongom.jpg" },
-  { id: "pres-2", name: "KIRABO MARYANNE HILDA", position: "PRESIDENT", photo: "/images/candidates/president-kirabo-maryanne-hilda.jpg" },
+  // PRESIDENT
+  { id: "president-kayla", name: "KAYLA ABEJA ONGOM", position: "PRESIDENT", photo: "/images/candidates/Kayla_Abeja_Ongom.jpeg" },
+  { id: "president-kirabo", name: "KIRABO MARYANNE HILDA", position: "PRESIDENT", photo: "/images/candidates/Kirabo_Maryanne_Hilda.jpeg" },
 
-  // VICE PRESIDENT - 2
-  { id: "vpres-1", name: "AUSTIN LYLE AGABA", position: "VICE PRESIDENT", photo: "/images/candidates/vice-president-austin-lyle-agaba.jpg" },
-  { id: "vpres-2", name: "KAZIBWE GRACE AINEBYONA", position: "VICE PRESIDENT", photo: "/images/candidates/vice-president-kazibwe-grace-ainebyona.jpg" },
+  // VICE PRESIDENT
+  { id: "vice-austin", name: "AUSTIN LYLE AGABA", position: "VICE PRESIDENT", photo: "/images/candidates/austin_lyle_agaba.jpeg" },
+  { id: "vice-kazibwe", name: "KAZIBWE GRACE AINEBYONA", position: "VICE PRESIDENT", photo: "/images/candidates/kazibwe_grace_ainebyona.jpeg" },
 
-  // LITURGY MINISTER - YES/NO
-  { id: "liturgy-yes", name: "JARON WYNN BULAMU - YES", position: "LITURGY MINISTER", photo: "/images/candidates/liturgy-jaron-wynn-bulamu.jpg" },
-  { id: "liturgy-no", name: "JARON WYNN BULAMU - NO", position: "LITURGY MINISTER", photo: "/images/candidates/liturgy-jaron-wynn-bulamu.jpg" },
+  // LITURGY MINISTER (from your earlier photo list)
+  { id: "liturgy-jaron", name: "JARON WYNN BULAMU", position: "LITURGY MINISTER", photo: "/images/candidates/jaron_wynn_bulamu.jpeg" },
+  { id: "liturgy-jeremiah", name: "JEREMIAH JOSIAH MWINE", position: "LITURGY MINISTER", photo: "/images/candidates/jeremiah_josiah_mwine.jpeg" },
 
-  // VICE LITURGY MINISTER - YES/NO
-  { id: "vliturgy-yes", name: "ALEXA DARLENE MUSUNGA - YES", position: "VICE LITURGY MINISTER", photo: "/images/candidates/vice-liturgy-alexa-darlene-musunga.jpg" },
-  { id: "vliturgy-no", name: "ALEXA DARLENE MUSUNGA - NO", position: "VICE LITURGY MINISTER", photo: "/images/candidates/vice-liturgy-alexa-darlene-musunga.jpg" },
+  // GENERAL SECRETARY / SPEAKER / OTHER - you can change position text
+  { id: "gen-kironde", name: "KIRONDE DARIAN", position: "GENERAL SECRETARY", photo: "/images/candidates/kironde_darian.jpeg" },
+  { id: "gen-mangeni", name: "MANGENI JESSE REUBEN", position: "GENERAL SECRETARY", photo: "/images/candidates/mangeni_jesse_reuben.jpeg" },
+  { id: "gen-martina", name: "MARTINA MARY NAMUGENYI", position: "GENERAL SECRETARY", photo: "/images/candidates/martina_mary_namugenyi.jpeg" },
+  { id: "gen-masaba", name: "MASABA MOSES", position: "GENERAL SECRETARY", photo: "/images/candidates/masaba-moses.jpeg" },
 
-  // ACADEMIC AND VOCATIONAL MINISTER - 2
-  { id: "acad-1", name: "ALMA MONA KAFUKO", position: "ACADEMIC AND VOCATIONAL MINISTER", photo: "/images/candidates/academic-alma-mona-kafuko.jpg" },
-  { id: "acad-2", name: "NOELLA FAVOUR MIREMBE", position: "ACADEMIC AND VOCATIONAL MINISTER", photo: "/images/candidates/academic-noella-favour-mirembe.jpg" },
-
-  // HEALTH MINISTER - YES/NO
-  { id: "health-yes", name: "KYLER AWOR ABISAG - YES", position: "HEALTH MINISTER", photo: "/images/candidates/health-kyler-awor-abisag.jpg" },
-  { id: "health-no", name: "KYLER AWOR ABISAG - NO", position: "HEALTH MINISTER", photo: "/images/candidates/health-kyler-awor-abisag.jpg" },
-
-  // EVENT MINISTER - 2
-  { id: "event-1", name: "MIKISA AVNIEL", position: "EVENT MINISTER", photo: "/images/candidates/event-mikisa-avniel.jpg" },
-  { id: "event-2", name: "ISAIAH ROYAL WALUSIMBI", position: "EVENT MINISTER", photo: "/images/candidates/event-isaiah-royal-walusimbi.jpg" },
-
-  // MEDIA AND COMMUNICATION MINISTER - 2
-  { id: "media-1", name: "THANDIWE ARINDA", position: "MEDIA AND COMMUNICATION MINISTER", photo: "/images/candidates/media-thandiwe-arinda.jpg" },
-  { id: "media-2", name: "JEREMIAH JOSIAH MWINE", position: "MEDIA AND COMMUNICATION MINISTER", photo: "/images/candidates/media-jeremiah-josiah-mwine.jpg" },
-
-  // CAFETERIA MINISTER - YES/NO
-  { id: "cafe-yes", name: "MARTINA MARY NAMUGENYI - YES", position: "CAFETERIA MINISTER", photo: "/images/candidates/cafeteria-martina-mary-namugenyi.jpg" },
-  { id: "cafe-no", name: "MARTINA MARY NAMUGENYI - NO", position: "CAFETERIA MINISTER", photo: "/images/candidates/cafeteria-martina-mary-namugenyi.jpg" },
-
-  // ASSISTANT CAFETERIA MINISTER - YES/NO
-  { id: "acafe-yes", name: "MICHELLE EPITSAM KIKOBYE - YES", position: "ASSISTANT CAFETERIA MINISTER", photo: "/images/candidates/assistant-cafeteria-michelle-epitsam-kikobye.jpg" },
-  { id: "acafe-no", name: "MICHELLE EPITSAM KIKOBYE - NO", position: "ASSISTANT CAFETERIA MINISTER", photo: "/images/candidates/assistant-cafeteria-michelle-epitsam-kikobye.jpg" },
-
-  // UNIFORM MINISTER - YES/NO
-  { id: "uniform-yes", name: "SEAN KING MULIIKA - YES", position: "UNIFORM MINISTER", photo: "/images/candidates/uniform-sean-king-muliika.jpg" },
-  { id: "uniform-no", name: "SEAN KING MULIIKA - NO", position: "UNIFORM MINISTER", photo: "/images/candidates/uniform-sean-king-muliika.jpg" },
-
-  // TIME KEEPER - 3
-  { id: "time-1", name: "MANGENI JESSE REUBEN", position: "TIME KEEPER", photo: "/images/candidates/time-keeper-mangeni-jesse-reuben.jpg" },
-  { id: "time-2", name: "CLARA ANNE ACERE", position: "TIME KEEPER", photo: "/images/candidates/time-keeper-clara-anne-acere.jpg" },
-  { id: "time-3", name: "ZION MICHEAL AMPEIRE", position: "TIME KEEPER", photo: "/images/candidates/time-keeper-zion-micheal-ampeire.jpg" },
-
-  // SPORTS MINISTER - 2
-  { id: "sports-1", name: "JEREMY SANYU KYOBE", position: "SPORTS MINISTER", photo: "/images/candidates/sports-jeremy-sanyu-kyobe.jpg" },
-  { id: "sports-2", name: "AHEREZA SHEBAH", position: "SPORTS MINISTER", photo: "/images/candidates/sports-ahereza-shebah.jpg" },
-
-  // ENVIRONMENT MINISTER - 3
-  { id: "env-1", name: "ANABO HAZEL", position: "ENVIRONMENT MINISTER", photo: "/images/candidates/environment-anabo-hazel.jpg" },
-  { id: "env-2", name: "MASABA MOSES", position: "ENVIRONMENT MINISTER", photo: "/images/candidates/environment-masaba-moses.jpg" },
-  { id: "env-3", name: "KIRONDE DARIAN", position: "ENVIRONMENT MINISTER", photo: "/images/candidates/environment-kironde-darian.jpg" },
+  { id: "pref-sean", name: "SEAN KING MULIIKA", position: "TIME KEEPER", photo: "/images/candidates/sean_king_muliika.jpeg" },
+  { id: "pref-thandiwe", name: "THANDIWE ARINDA", position: "TIME KEEPER", photo: "/images/candidates/thandiwe_arinda.jpeg" },
+  { id: "pref-alexa", name: "ALEXA DARLENE MUSUNGA", position: "PREFECT", photo: "/images/candidates/alexa_darlene_musunga.jpeg" },
+  { id: "pref-alma", name: "ALMA MONA KAFUKO", position: "PREFECT", photo: "/images/candidates/alma_mona_kafuko.jpeg" },
+  { id: "pref-anabo", name: "ANABO HAZEL", position: "PREFECT", photo: "/images/candidates/anabo_hazel.jpeg" },
+  { id: "pref-clara", name: "CLARA ANNE ACERE", position: "PREFECT", photo: "/images/candidates/clara_anne_acere.jpeg" },
 ];
